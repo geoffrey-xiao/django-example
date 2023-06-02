98383d9280e4acda9d49b90060f60c292934c284
xiaojianfeng <1377499035@qq.com>
2023-06-03 01:08:09 +0800

    factory

--- tests/factories.py
+++ tests/factories.py
@@ -1,6 +1,16 @@
 
 import factory
 from projects import models
+from django.contrib.auth.models import User
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    id = factory.Sequence(lambda n: n+1)
+    username = factory.LazyAttribute(lambda: f'user{id}')
+    email = factory.LazyAttribute(lambda: f'{username}@email.com')
 
 
 class TagFactory(factory.django.DjangoModelFactory):
